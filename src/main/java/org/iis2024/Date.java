5166074fdd5086c0b4277b5c074a82b905d75ab0
AS82mr <aryansepasi@gmail.com>
2025-03-12 14:14:46 +0100

    added Step 1 code of exe 7

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -44,6 +44,8 @@ public class Date {
    *
    * @return The year (within the valid range of 1900 to 2050).
    */
+
+
   public int getYear() {
     return year;
   }
@@ -60,10 +62,17 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+// Constructor modified by the owner
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
+  }
+  // Constructor modified by the collaborator
+  public Date(int tag, int monat, int jahr) {
+    this.day = tag;
+    this.month = monat;
+    this.year = jahr;
   }
 
   /**
